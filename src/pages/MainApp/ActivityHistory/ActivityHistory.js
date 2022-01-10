8b4fe1889ccc953c195f3870187c8111b4ce0a4c
yigityilmaz-m <yigityilmaz.m@gmail.com>
2022-01-10 05:25:25 +0300

    personalBestSpeed calculated

--- src/pages/MainApp/ActivityHistory/ActivityHistory.js
+++ src/pages/MainApp/ActivityHistory/ActivityHistory.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect ,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text } from 'react-native';
 import database from '@react-native-firebase/database';
 import ActivityHistoryCard from '../../../components/ActivityHistoryCard';
@@ -8,37 +8,36 @@ import { UserContext } from '../../../context/UserProvider';
 export default function ActivityHistory() {
   const [userActivities, setUserActivities] = useState([])
   const { state } = useContext(UserContext)
+  const [bestSpeed, setBestSpeed] = useState(0)
 
   useEffect(() => {
     listenActivityChanges()
   }, [])
 
-
   function listenActivityChanges() {
-    
     database()
       .ref(`users/${state.userId}/activities`)
       .on('value', snapshot => {
         const activity = snapshot.val();
         if (!!activity) {
-          setUserActivities(Object.keys(activity).map(k => ({
+          let ActivityArray = Object.keys(activity).map(k => ({
             id: k,
             ...activity[k]
-          })
-          ))
+          }))
+          let SpeedArray = ActivityArray.map(item => [item.speed])
+          setBestSpeed(Math.max.apply(Math, SpeedArray))
+          setUserActivities(ActivityArray)
         }
       })
-
   }
 
-  const renderItem = ({ item }) => (
-    <View>
-      <ActivityHistoryCard activ={item} />
-    </View>
-  );
 
   return (
     <View>
+      <View>
+        <Text>{bestSpeed}</Text>
+      </View>
+
       {!!userActivities.length ?
         (userActivities.map(b => (
           <ActivityHistoryCard activ={b} key={b.id} />
